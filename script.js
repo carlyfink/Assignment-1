fa93cf5f7957f4485a1717760ca2fda67f144493
Carly Fink <carlysfink@gmail.com>
2016-02-01 20:46:40 -0500

    html

---
+++ script.js
@@ -0,0 +1,21 @@
+
+    var favoritethings = ["run", "bike", "explore", "read", "bake"];
+    
+   
+ //whit a while loop  	
+   	var favoritethingsNum = 0
+   	while (favoritethingsNum < favoritethings.length) {
+   		console.log(favoritethings[favoritethingsNum]); 
+   		favoritethingsNum++;
+   	} 
+
+//with a for loop
+   	for(var favoritethingsNum=0; favoritethingsNum < favoritethings.length; favoritethingsNum++) {
+   		console.log(favoritethings[favoritethingsNum])
+   	}
+    
+
+
+
+
+
